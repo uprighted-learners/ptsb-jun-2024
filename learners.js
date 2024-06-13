b95a2d0e4214fedb995581dbb91c12162e93a937
Danny Burrow <burrowingdan@gmail.com>
2024-06-12 17:51:26 -0700

    add stub learners random picker

---
+++ learners.js
@@ -0,0 +1,16 @@
+const students = [
+    "Adeyemi",
+    "Rey",
+    "Unique",
+    "Chris",
+    "Tanner",
+    "Betty",
+    "Lucy",
+    "Samuel",
+    "Aaron",
+    "Ricardo",
+    "Jasmine",
+    "John",
+    "Urmi",
+    "Paulo",
+]
