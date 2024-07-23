81c994a467cba899ada3e53bed0049005eeca5f1
Danny Burrow <burrowingdan@gmail.com>
2024-07-22 17:02:47 -0700

    create project structure

---
+++ 4-web-apis/event-listeners/app.js
@@ -0,0 +1,20 @@
+/* 
+    ? Event Listeners
+    * listens for input from users
+        * inherently async, event can happen any time
+    * always listen for one single type of event
+    * can listen for any kind of event
+        * clicks
+        * key strokes
+        * specific chars or chords
+        * mouse hover events
+        * input changes
+        * https://www.w3schools.com/jsref/dom_obj_event.asp
+    * always attached to a specific element
+    * take a callback, the callback runs when the event happens
+*/
+
+// goal: make button turn the box red
+// select the box
+// select the button
+// add event listener to button
