5fdb5581b0b647fb3eba5b957aaf8310ed32be13
Danny Burrow <burrowingdan@gmail.com>
2024-07-24 17:23:50 -0700

    build vote counter for genres

--- 4-web-apis/poll/app.js
+++ 4-web-apis/poll/app.js
@@ -1,1 +1,19 @@
-// goal 1:
+// goal 1: when we click the klezmer button, increment klezmer count
+// select the klezmer button
+// add event listener to it
+// select the klezmer count
+// increment klezmer count (this happens in the event listener)
+
+// all query selectors together at the top, for tidiness
+
+const genres = ["klezmer", "alt", "rb", "edm", "jazz", "yodel"];
+
+genres.forEach((title) => {
+  const button = document.getElementById(`${title}-button`);
+  const count = document.getElementById(`${title}-count`);
+
+  button.addEventListener("click", () => {
+    let num = Number(count.textContent);
+    count.textContent = num + 1;
+  });
+});
