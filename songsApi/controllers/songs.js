c1e49e2910e52a38a92d93b48bb5e2be5c9927f0
Danny Burrow <burrowingdan@gmail.com>
2024-08-15 18:18:07 -0700

    add new enpoints to songs API

--- songsApi/controllers/songs.js
+++ songsApi/controllers/songs.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const path = require("path")
 const fs = require("fs")
-const { release } = require("os")
 
 // global constant
 const SONGS_PATH = path.join(__dirname, "../static/songs.json")
@@ -28,6 +27,32 @@ router.get("/catalog/:songId", (req, res) => {
   // TODO: what to send when no result?
 })
 
+router.get("/search", (req, res) => {
+  const genres = req.query.genre.split(",")
+  const songs = getJSON()
+
+  // TODO: extra point on any assignment for whoever solves this
+  // goal: return any song that includes ALL comma separated genres from query
+  // localhost:4000/search?genre=rap,hip-hop
+  const matches = songs.filter((s) => {
+    const lowerCaseGenres = s.genre.map((g) => g.toLowerCase())
+    return genres.every((g) => {
+      console.log(g)
+      lowerCaseGenres.includes(g)
+    })
+  })
+  res.send(matches)
+
+  // This is the working version for a single genre search
+  // const genre = req.query.genre
+  // const songs = getJSON()
+  // const matches = songs.filter((s) => {
+  //   const lowerCaseGenres = s.genre.map((g) => g.toLowerCase())
+  //   return lowerCaseGenres.includes(genre)
+  // })
+  // res.send(matches)
+})
+
 const getNewId = () => {
   const songs = getJSON()
   const lastSong = songs[songs.length - 1]
@@ -50,7 +75,30 @@ router.post("/add", (req, res) => {
   res.send("song added")
 })
 
-// TODO: put
-// TODO: delete
+router.put("/update/:songId", (req, res) => {
+  const songId = req.params.songId
+  const songs = getJSON()
+  const song = songs.find((s) => s.id == songId)
+  const index = songs.indexOf(song)
+
+  let newSong = Object.assign(song, req.body)
+  songs[index] = newSong
+
+  fs.writeFileSync(SONGS_PATH, JSON.stringify(songs))
+  res.send("song updated")
+  // TODO: prevent bad fields
+})
+
+router.delete("/delete/:songId", (req, res) => {
+  const songId = req.params.songId
+  const songs = getJSON()
+  const song = songs.find((s) => s.id == songId)
+  const index = songs.indexOf(song)
+
+  songs.splice(index, 1)
+
+  fs.writeFileSync(SONGS_PATH, JSON.stringify(songs))
+  res.send("song deleted")
+})
 
 module.exports = router
