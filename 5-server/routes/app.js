220a7a761490f8ba8043be00ad39ae177dcd9567
Danny Burrow <burrowingdan@gmail.com>
2024-08-15 18:20:08 -0700

    update notes

--- 5-server/routes/app.js
+++ 5-server/routes/app.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-const PORT = 4000;
+const PORT = 4000
 
 // this middleware encodes submitted data in a readable way
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // a single slash is the "root"
 // all route urls must start with a slash
 app.get("/", (req, res) => {
-  res.send("this is the root");
-});
+  res.send("this is the root")
+})
 
 // the first argument is the path
 // the path part of the url is everything following the top-level-domain
 // or, when developing locally, everything following the host:port
 app.get("/about", (req, res) => {
-  res.send("this is the about page, its address is hard-coded");
-});
+  res.send("this is the about page, its address is hard-coded")
+})
 
 // always prefix routes that include url parameters with something hard-coded
 // to prevent accidentally catching other routes
 app.get("/name/:firstName/:lastName", (req, res) => {
-  res.send(`Nice to meet you ${req.params.firstName} ${req.params.lastName}`);
-});
+  res.send(`Nice to meet you ${req.params.firstName} ${req.params.lastName}`)
+})
 
 // we can also use query strings to pass information from the url
 app.get("/name", (req, res) => {
   // localhost:4000/name?anyKey=anyValue&lastName=Burrow&firstName=Danny
-  console.log(req.query);
-  res.send(`Nice to meet you ${req.query.firstName} ${req.query.lastName}`);
-});
+  console.log(req.query)
+  res.send(`Nice to meet you ${req.query.firstName} ${req.query.lastName}`)
+})
 
 // this middelware line lets us parse the request body as JSON
-app.use(express.json());
+app.use(express.json())
 // there's another middleware (at the top) that lets us encode form data
 app.post("/name", (req, res) => {
-  console.log(req.body);
+  console.log(req.body)
   res.send(
     `${req.body.firstName} ${req.body.lastName} saved to DB (for pretend)`
-  );
-});
+  )
+})
 
 app.get("/input", (req, res) => {
   // we can send raw HTML in the response and it will be rendered!
@@ -52,11 +52,11 @@ app.get("/input", (req, res) => {
       <input type="text" name="lastName" placeholder="last name">
       <input type="submit" value="Click me">
     </form>
-    `);
-});
+    `)
+})
 
 app.get("/color", (req, res) => {
-  console.log(req.query);
+  console.log(req.query)
   // when we hit this endpoint with a color hex code as a query parameter
   // the background color is set to that hex code
   res.send(`
@@ -66,9 +66,9 @@ app.get("/color", (req, res) => {
         <button type="submit">Click me</button>
       </form>
     </body>
-    `);
-});
+    `)
+})
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+  console.log(`listening on port ${PORT}`)
+})
