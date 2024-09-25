2805e0fa0d55dcffc3a16e624dbfa973708bc7b5
Danny Burrow <burrowingdan@gmail.com>
2024-09-24 17:05:02 -0700

    final working day

--- songs-client/src/components/SongCard.js
+++ songs-client/src/components/SongCard.js
@@ -1,8 +1,10 @@
 import "./songCard.css"
+import { useNavigate } from "react-router-dom"
 
 function SongCard({ song }) {
+  const nav = useNavigate()
   return (
-    <div className="song-card">
+    <div className="song-card" onClick={() => nav(`detail/${song._id}`)}>
       <div className="title">{song.title}</div>
       <div>{song.artist}</div>
     </div>
